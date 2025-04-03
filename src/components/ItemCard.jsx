3f005b28dbeb72992ed5ba6108943c086a076bf6
mradul kumar katiyar <dev.mradul@proton.me>
2025-04-03 20:41:14 +0530

    Enhance ItemCard component: increase height, improve layout, and adjust button positioning

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,17 +6,22 @@ const ItemCard = ({ imgPath, title, content }) => {
     <Paper
       className="md:w-2/5 lg:w-1/4  mx-4 shadow-2xl my-4 p-4 border-1 border-gray-300"
       radius={10}
-      h={500}
+      h={600}
+      shadow="lg"
     >
-      <Image src={imgPath} radius={10} h={250} w="100%" />
-      <div className="mt-4">
-        <Title>{title}</Title>
-        <Text>{content}</Text>
+      <div className="min-h-full min-w-full flex flex-col items-center justify-between">
         <div className="">
-          <Button w="100%" className="w-full mt-4">
-            Start learn
-          </Button>
+          <Image src={imgPath} radius={10} h={250} w="100%" />
+          <div className="mt-4">
+            <Title>{title}</Title>
+            <Text mt={10}>{content}</Text>
+          </div>
         </div>
+          <div className="w-full">
+            <Button w="100%" className="w-full">
+              Start learn
+            </Button>
+          </div>
       </div>
     </Paper>
   );
